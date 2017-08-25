2decc810f92ef56d3fbb82353bd439e4ff66e761
Emil Ekman <Tangooe@gmail.com>
2017-08-25 12:44:20 +0200

    Added error messages to DishFormView Model + styling to form

--- PizzeriaButikenOnline/PizzeriaButikenOnline/View Models/DishFormViewModel.cs
+++ PizzeriaButikenOnline/PizzeriaButikenOnline/View Models/DishFormViewModel.cs
@@ -6,20 +6,20 @@ namespace PizzeriaButikenOnline.View_Models
 {
     public class DishFormViewModel
     {
-        [Required]
-        [StringLength(50)]
+        [Required(ErrorMessage = "Namnet måste vara ifyllt")]
+        [StringLength(50, ErrorMessage = "Namnet får inte vara längre än 50 bokstäver")]
         public string Name { get; set; }
 
-        [Required]
-        [Range(0, 5000)]
+        [Required(ErrorMessage = "Priset måste vara ifyllt")]
+        [Range(1, 5000,ErrorMessage = "Priset måste vara mellan 1-5000kr")]
         public decimal Price { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "En kategori måste vara vald")]
         public int Category { get; set; }
 
         public IList<Category> Categories { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Minst en ingridiens måste vara vald")]
         public IList<int> Ingredient { get; set; }
 
         public IList<Ingredient> Ingredients { get; set; }
