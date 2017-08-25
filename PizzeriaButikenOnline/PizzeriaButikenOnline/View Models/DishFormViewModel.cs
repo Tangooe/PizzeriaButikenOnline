ec6fab4cc79119df924f8dded93b3c51ceed5fc0
Emil Ekman <Tangooe@gmail.com>
2017-08-25 12:46:58 +0200

    Added DisplayName to DishFormViewModel

--- PizzeriaButikenOnline/PizzeriaButikenOnline/View Models/DishFormViewModel.cs
+++ PizzeriaButikenOnline/PizzeriaButikenOnline/View Models/DishFormViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using PizzeriaButikenOnline.Models;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace PizzeriaButikenOnline.View_Models
@@ -8,18 +9,22 @@ namespace PizzeriaButikenOnline.View_Models
     {
         [Required(ErrorMessage = "Namnet måste vara ifyllt")]
         [StringLength(50, ErrorMessage = "Namnet får inte vara längre än 50 bokstäver")]
+        [DisplayName("Namn")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Priset måste vara ifyllt")]
         [Range(1, 5000,ErrorMessage = "Priset måste vara mellan 1-5000kr")]
+        [DisplayName("Pris")]
         public decimal Price { get; set; }
 
         [Required(ErrorMessage = "En kategori måste vara vald")]
+        [DisplayName("Kategori")]
         public int Category { get; set; }
 
         public IList<Category> Categories { get; set; }
 
         [Required(ErrorMessage = "Minst en ingridiens måste vara vald")]
+        [DisplayName("Ingridienser")]
         public IList<int> Ingredient { get; set; }
 
         public IList<Ingredient> Ingredients { get; set; }
