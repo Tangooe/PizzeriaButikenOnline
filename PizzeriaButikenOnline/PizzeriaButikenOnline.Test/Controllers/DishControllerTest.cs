c6c3e5185b6c31eeed246d1b4bcaa6ff826eb3cb
Emil Ekman <tangooe@gmail.com>
2017-09-14 07:28:16 +0200

    Added UnitTesting

---
+++ PizzeriaButikenOnline/PizzeriaButikenOnline.Test/Controllers/DishControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PizzeriaButikenOnline.Controllers;
+using PizzeriaButikenOnline.Core;
+using PizzeriaButikenOnline.Core.Repositories;
+
+namespace PizzeriaButikenOnline.Test.Controllers
+{
+    [TestClass]
+    public class DishControllerTest
+    {
+        private readonly DishController _controller;
+
+        public DishControllerTest()
+        {
+            var mockRepository = new Mock<IDishRepository>();
+            var mockUoW = new Mock<IUnitOfWork>();
+            mockUoW.SetupGet(u => u.Dishes).Returns(mockRepository.Object);
+
+            var mockMapper = new Mock<IMapper>();
+            _controller = new DishController(mockMapper.Object, mockUoW.Object);
+        }
+
+        [TestMethod]
+        public void Delete_NoDishWithGivenIdExists_ShouldReturnNotFound()
+        {
+            _controller.Delete(1).Should().BeOfType<NotFoundResult>();
+        }
+    }
+}
---
+++ PizzeriaButikenOnline/PizzeriaButikenOnline.Test/Models/CartTest.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PizzeriaButikenOnline.Core.Models;
+using PizzeriaButikenOnline.Core.ViewModels;
+using System.Collections.Generic;
+
+namespace PizzeriaButikenOnline.Test.Models
+{
+    [TestClass]
+    public class CartTest
+    {
+        private readonly Cart _cart;
+        public CartTest()
+        {
+            _cart = new Cart();
+            _cart.AddItem(new DishViewModel
+            {
+                Id = 1,
+                Category = new Category
+                {
+                    Id = 1,
+                    Name = "Pizzor"
+                },
+                DishNumber = 1,
+                Name = "Margharita",
+                Price = 70,
+                Ingredients = new List<IngredientViewModel>
+                {
+                    new IngredientViewModel
+                    {
+                        Id = 1,
+                        IsDefault = true,
+                        IsSelected = true,
+                        Name = "Ost",
+                        Price = 5
+                    },
+                    new IngredientViewModel
+                    {
+                        Id = 1,
+                        IsDefault = true,
+                        IsSelected = true,
+                        Name = "Tomatsås",
+                        Price = 5
+                    }
+                }
+            }, 1);
+            _cart.AddItem(new DishViewModel
+            {
+                Id = 2,
+                Category = new Category
+                {
+                    Id = 1,
+                    Name = "Pizzor"
+                },
+                DishNumber = 1,
+                Name = "TorrPizza",
+                Price = 70,
+                Ingredients = new List<IngredientViewModel>
+                {
+                    new IngredientViewModel
+                    {
+                        Id = 1,
+                        IsDefault = true,
+                        IsSelected = true,
+                        Name = "Ost",
+                        Price = 5
+                    },
+                    new IngredientViewModel
+                    {
+                        Id = 3,
+                        IsDefault = true,
+                        IsSelected = true,
+                        Name = "Skinka",
+                        Price = 5
+                    }
+                }
+            }, 1);
+        }
+
+        [TestMethod]
+        public void ComputeTotalValue_TotalDishValue140_ShouldReturn140()
+        {
+            _cart.ComputeTotalValue().Should().Be(140);
+        }
+    }
+}
